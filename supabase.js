26a0d4bd6653eacfeea3a92f44821e595b18b3b2
provencegreniers-ship-it <provencegreniers@gmail.com>
2026-08-19 10:18:17 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Utilise la clé "service_role" côté serveur uniquement (jamais exposée au navigateur)
-export const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
